363d289281eacfb82d797b86ecd56382b38a881e
moneebshammout <moneeb41@gmail.com>
2023-08-02 14:36:37 +0300

    early setup

---
+++ backend/custom_user/models.py
@@ -0,0 +1,56 @@
+# custom_users/models.py
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True)
+    birthdate = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    points = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="images/")
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name","phone", "image"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+"""
+Table user {
+  role varchar
+  level_id integer TODO: add level table then append it here
+}
+"""
