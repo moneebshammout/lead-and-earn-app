2f8aeac4ff349f16d1a4bb663d609fa1d183c2b1
moneebshammout <moneeb41@gmail.com>
2023-08-04 22:52:29 +0300

    added docker and docker compose

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,21 @@ export default ({ command, mode }) => {
   const port = process.env.VITE_PORT || 3000;
   return {
     server: {
+      host: '0.0.0.0',
       port,
     },
     plugins: [react()],
+    host: true, // Here
+    build: {
+      rollupOptions: {
+        external: [
+          '@mui/base/ModalUnstyled',
+          '@mui/base/BadgeUnstyled',
+          '@mui/base/PopperUnstyled',
+          '@mui/base/SliderUnstyled',
+          '@mui/base/AutocompleteUnstyled',
+        ],
+      },
+    },
   };
 };
